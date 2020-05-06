6e9a2ee408878841253537217ef54250c0ef1964
Seth Todd <seth.todd@outlook.com>
2020-05-06 02:58:29 +0000

    first commit

---
+++ lib/s/cli.rb
@@ -0,0 +1,8 @@
+require "s/cli/version"
+
+module S
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/s/cli/version.rb
@@ -0,0 +1,5 @@
+module S
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
